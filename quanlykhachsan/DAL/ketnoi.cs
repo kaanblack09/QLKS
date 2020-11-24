daa327e7e7b9d117a6e7df16736021195c2e8643
NguyenQuocKhanh <44228638+kaanblack09@users.noreply.github.com>
2020-11-24 20:10:07 +0700

    169
    ae clone ve

---
+++ quanlykhachsan/DAL/ketnoi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class ketnoi
+    {
+        public static SqlConnection Get()
+        {
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=quanlykhachsan1;Integrated Security=True";
+            cnn.Open();
+            return cnn;
+        }
+   }
+}
